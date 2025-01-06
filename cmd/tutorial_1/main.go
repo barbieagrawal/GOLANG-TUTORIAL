3c422a0a05a5d9b3a0cd011851b63cc90f6f762c
barbieagrawal <barbieagrawal1912@gmail.com>
2025-01-06 13:21:00 +0530

    arrays and slices added

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -39,12 +39,36 @@ func main() {
 
 	switch remainder { //conditional switch statements
 	case 0:
-		fmt .Printf("The division was exact")
+		fmt .Printf("The division was exact\n")
 	case 1,2: 
-		fmt.Printf("The division was close")
+		fmt.Printf("The division was close\n")
 	default:
-		fmt.Printf("The division was not close")
+		fmt.Printf("The division was not close\n")
 	}
+
+	var arr [3]int32 //by default array elements initialized to 0
+	arr[1] = 123
+	fmt.Println(arr[0]) //first element 
+	fmt.Println(arr[1:3]) //second and third elements 
+
+	fmt.Println(&arr[0]) //location in continguous memory alloc of array
+	fmt.Println(&arr[1])
+	fmt.Println(&arr[2])
+
+	var arr2 [3]int32 = [3]int32{1, 2, 3} //a way of initialization
+	fmt.Println(arr2[0:3])
+
+	arr3 := [...]int32{4, 5, 6}
+	fmt.Println(arr3)
+
+	//arrays have fixed size, slices can have variable sizes
+
+	var sl []int32 = []int32{7, 8, 9}
+	fmt.Println(sl)
+	fmt.Printf("The length is %v with capacity %v\n", len(sl), cap(sl))
+	sl = append(sl, 10)
+	fmt.Println(sl)
+	fmt.Printf("The length is %v with capacity %v\n", len(sl), cap(sl))
 }
 
 func printMe() {
